e5e6d2530be1fdcf32f571adbb156597fcbe11b2
hrdrq <son.munehiro@gmail.com>
2024-11-03 18:46:19 +0900

    Add SetDueDate() and update notesInfo()

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1648,6 +1648,11 @@ class AnkiConnect:
         return result
 
 
+    @util.api()
+    def setDueDate(self, cards, days):
+        self.scheduler().set_due_date(cards, days, config_key=None)
+        return True
+
 
     @util.api()
     def reloadCollection(self):
@@ -1673,7 +1678,13 @@ class AnkiConnect:
 
 
     @util.api()
-    def notesInfo(self, notes):
+    def notesInfo(self, notes=None, query=None):
+        if notes is None and query is None:
+            raise Exception('Must provide either "notes" or a "query"')
+        
+        if query is not None:
+            notes = self.findNotes(query)
+
         result = []
         for nid in notes:
             try:
